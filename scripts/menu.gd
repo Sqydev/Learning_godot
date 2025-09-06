4eb430ff260fc9d17f5652301a02b0f0f61ed871
Sqyd <wojkap10@proton.me>
2025-09-06 14:57:03 +0200

    made very basic menu

---
+++ scripts/menu.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var play = $Play
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	if(play.button_pressed): get_tree().change_scene_to_file("res://scenes/Level1.tscn")
